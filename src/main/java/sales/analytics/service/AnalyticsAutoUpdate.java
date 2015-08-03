d1b69d723fbad8345b13118f6d4da0b3d2235d51
MyroslavDerevyanko <muroclav94@ukr.net>
2015-08-03 14:47:06 +0300

    added  on start generator

--- src/main/java/sales/analytics/service/AnalyticsAutoUpdate.java
+++ src/main/java/sales/analytics/service/AnalyticsAutoUpdate.java
@@ -5,7 +5,11 @@ import org.springframework.scheduling.annotation.Scheduled;
 import sales.analytics.domain.Analytics;
 import sales.analytics.repository.AnalyticsRepository;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Random;
 
 /**
  * Created by Myroslav on 30.07.2015.
@@ -15,10 +19,14 @@ public class AnalyticsAutoUpdate {
     private AnalyticsService analyticsService;
     private AnalyticsRepository analyticsRepository;
 
-    @Scheduled(cron="50 23 * * * ?")
-    public void demoServiceMethod()
+    @Scheduled(cron="23 50 * * * ?")
+    public void dailyUpdate()
     {
-        System.out.println("Fake date added");
-        analyticsService.save(new Analytics(5, 6000, 10, 15, new Date()));
+        Random rand = new Random();
+        System.out.println("Daily update" + new Date());
+        analyticsRepository.save(new Analytics(100+rand.nextInt(500), 20000+rand.nextInt(980000), 100 + rand.nextInt(700), 15+rand.nextInt(85), new Date()));
     }
+
+
+
 }
--- src/main/java/sales/analytics/service/AnalyticsServiceImpl.java
+++ src/main/java/sales/analytics/service/AnalyticsServiceImpl.java
@@ -7,9 +7,7 @@ import sales.analytics.domain.Analytics;
 import sales.analytics.repository.AnalyticsRepository;
 
 import javax.transaction.Transactional;
-import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by Myroslav on 28.07.2015.
@@ -30,6 +28,9 @@ public class AnalyticsServiceImpl implements AnalyticsService{
 
     @Override
     public List<Analytics> getAll() {
+        if(tableIsEmpty()) {
+            createOnStart();
+        }
         return analyticsRepository.findAll();
     }
 
@@ -37,6 +38,27 @@ public class AnalyticsServiceImpl implements AnalyticsService{
     public Analytics save(Analytics analytics) {
         analyticsRepository.save(analytics);
         return analytics;
+    }
+
+    public void createOnStart()
+    {
+        for(int i=30; i>0; i--) {
+            Calendar calendar = Calendar.getInstance();
+            calendar.add(Calendar.DATE, -i);
+            Random rand = new Random();
+            analyticsRepository.save(new Analytics(100+rand.nextInt(500), 20000+rand.nextInt(980000), 100 + rand.nextInt(700), 15+rand.nextInt(85), calendar.getTime()));
+            System.out.println("Fake date added");
+        }
+    }
+
+    public boolean tableIsEmpty()
+    {
+        if(analyticsRepository.findAll().size()==0)
+        {
+            System.out.println("Table is empty");
+            return true;
+        }
+        return false;
     }
 
 }
